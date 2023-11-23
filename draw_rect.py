3b44f3abbcec781000e3d10af65b02086522e225
MauBorre <122322896+MauBorre@users.noreply.github.com>
2023-11-23 15:36:54 -0300

    working

--- draw_rect.py
+++ draw_rect.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
 import imutils
+import pytesseract
+import tkinter as tk
+from tkinter import scrolledtext
 
 
+"""Rectangle drawing procedure"""
 drawing = False
 mode = True
-initial_x, initial_y = -1,-1
+initial_x, initial_y = 0,0
 ultimate_x, ultimate_y = 0,0
 
 def draw_rect(event,final_x,final_y,flags,param):
@@ -22,7 +26,6 @@ def draw_rect(event,final_x,final_y,flags,param):
     #elif event == cv2.EVENT_MOUSEMOVE:
         #if drawing == True:
             #if mode == True:
-                # Rectangulo feed
                 #cv2.rectangle(cache,(initial_x,initial_y),(final_x,final_y),(0,255,0), thickness=2)
                 #a=final_x
                 #b=final_y
@@ -34,7 +37,7 @@ def draw_rect(event,final_x,final_y,flags,param):
         """Rectangle finish"""
         drawing = False
         if mode == True:
-            cv2.rectangle(img_resize,(initial_x,initial_y),(final_x,final_y),(255,255,50), thickness=2) # CAPTURA FINAL
+            cv2.rectangle(img_resize,(initial_x,initial_y),(final_x,final_y),(255,255,50), thickness=2)
             ultimate_x, ultimate_y = final_x, final_y
             print(f"Initial points: {initial_x},{initial_y}")
             print(f"Final points: {ultimate_x},{ultimate_y}")
@@ -45,22 +48,69 @@ canvas.fill(255)
 
 """Image path and resizing utility"""
 img_path = "./image.jpg"
-img = cv2.imread(img_path)
-img_resize = imutils.resize(img, height=950)
 cv2.namedWindow('image')
+img = cv2.imread(img_path)
+img_resize = imutils.resize(img, height=1000)
 y=0
 x=0
 
-while(1):
+"""TKinter init"""
+root = tk.Tk()
+def close_window(event=None):
+    root.destroy()
+root.bind('<Escape>', close_window)
+text_widget = scrolledtext.ScrolledText(root)
+text_widget.pack()
 
-    cv2.setMouseCallback('image', draw_rect)
-    cv2.imshow('image',img_resize)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27: # ESCAPE KEY
-        break
-    if k == 32: # SPACE BAR KEY
-        print("cutting...")
-        cropImg = img_resize[initial_y:y+ultimate_y, initial_x:x+ultimate_x]
-        cv2.imshow("cropped", cropImg)
+"""Necessary tesseract config"""
+# If you don't have tesseract executable in your PATH, include the following:
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
+def main_loop():
+
+    while(1):
+
+        cv2.setMouseCallback('image', draw_rect)
+        cv2.imshow('image',img_resize)
+        k = cv2.waitKey(1) & 0xFF
+
+        if k == 27: # ESCAPE KEY
+            break
+
+        if k == 13: # ENTER KEY
+            print("OCR'ing...")
+            try:
+                cropImg = img_resize[initial_y:y+ultimate_y, initial_x:x+ultimate_x]
+                text = pytesseract.image_to_string(cropImg)
+                text_widget.insert(tk.END, text)
+                root.mainloop()
+            except:
+                text = pytesseract.image_to_string(img_resize)
+                text_widget.insert(tk.END, text)
+                root.mainloop()
+            # start_clean_again()
+
+        if k == 8: # BACKSPACE KEY
+            print("returning...")
+            # TODO: erease the rectangle or restart
+            """
+            Podemos usar cv2.imshow en dos imagenes unidas?
+            Y dibujar los rectangulos en la transparente
+            Al terminar de dibujar un rectangulo COPIAMOS y dibujamos sobre copia1
+            al tocar BACKSPACE volvemos a copia0 (sin rectangulo)"""
+            #main_loop()
+            
+
+
+main_loop()
 cv2.destroyAllWindows()
+
+
+
+"""
+Might be useful later on...
+if k == 32: # SPACE BAR KEY
+if k == 13: # ENTER KEY
+if k == 8: # RETURN KEY
+"""
